ff2de1edee879a41928c828e502afee3391ccec6
Daniel Van Cuylenburg <email@danielvanc.com>
2021-12-12 16:00:43 +0000

    feat: finish up showing jokes on pages

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,8 +1,31 @@
+import type { LoaderFunction } from "remix";
+import { useLoaderData, Link } from "remix";
+import { db } from "~/utils/db.server";
+import type { Joke } from "@prisma/client";
+
+type LoaderData = { randomJoke: Joke };
+
+export const loader: LoaderFunction = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  });
+
+  if (!randomJoke) throw new Error("No joke found");
+
+  const data: LoaderData = { randomJoke };
+  return data;
+};
+
 export default function JokesIndexRoute() {
+  const data = useLoaderData();
   return (
     <div>
-      <p>He'res a random joke</p>
-      <p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
+      <p>Here's a random joke:</p>
+      <p>{data.randomJoke.content}</p>
+      <Link to={data.randomJoke.id}>"{data.randomJoke.name}" Permalink</Link>
     </div>
   );
 }
