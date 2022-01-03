ccdf10585aa72241e66ad347218a6f6cfc50238f
Daniel Van Cuylenburg <email@danielvanc.com>
2022-01-03 12:56:55 +0000

    feat(auth): added authentication

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,5 +1,7 @@
 import type { LinksFunction, LoaderFunction } from "remix";
+import { User } from "@prisma/client";
 import { db } from "~/utils/db.server";
+import { getUser } from "~/utils/session.server";
 import { Link, Outlet, useLoaderData } from "remix";
 import styles from "../styles/jokes.css";
 
@@ -7,16 +9,20 @@ export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
 };
 
-type LoaderData = { jokeListItems: Array<{ id: string; name: string }> };
+type LoaderData = {
+  user: User | null;
+  jokeListItems: Array<{ id: string; name: string }>;
+};
 
-export let loader: LoaderFunction = async () => {
-  const data: LoaderData = {
-    jokeListItems: await db.joke.findMany({
-      take: 5,
-      select: { id: true, name: true },
-      orderBy: { createdAt: "desc" },
-    }),
-  };
+export const loader: LoaderFunction = async ({ request }) => {
+  const jokeListItems = await db.joke.findMany({
+    take: 5,
+    orderBy: { createdAt: "desc" },
+    select: { id: true, name: true },
+  });
+  const user = await getUser(request);
+
+  const data: LoaderData = { user, jokeListItems };
   return data;
 };
 
@@ -33,6 +39,18 @@ export default function Jokes() {
               <span className="logo-medium">J🤪KES</span>
             </Link>
           </h1>
+          {data.user ? (
+            <div className="user-info">
+              <span>{`Hi ${data.user.username}`}</span>
+              <form action="/logout" method="post">
+                <button type="submit" className="button">
+                  Logout
+                </button>
+              </form>
+            </div>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
         </div>
       </header>
       <main className="jokes-main">
---
+++ app/routes/login.tsx
@@ -0,0 +1,165 @@
+import type { ActionFunction, LinksFunction } from "remix";
+import { useActionData, json, Link, useSearchParams } from "remix";
+import { db } from "~/utils/db.server";
+import { createUserSession, login } from "~/utils/session.server";
+import stylesUrl from "../styles/login.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+function validateUsername(username: unknown) {
+  if (typeof username !== "string" || username.length < 3) {
+    return `Usernames must be at least 3 characters long`;
+  }
+}
+
+function validatePassword(password: unknown) {
+  if (typeof password !== "string" || password.length < 6) {
+    return `Passwords must be at least 6 characters long`;
+  }
+}
+
+type ActionData = {
+  formError?: string;
+  fieldErrors?: {
+    username: string | undefined;
+    password: string | undefined;
+  };
+  fields?: {
+    loginType: string;
+    username: string;
+    password: string;
+  };
+};
+
+const badRequest = (data: ActionData) => json(data, { status: 400 });
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const loginType = form.get("loginType");
+  const username = form.get("username");
+  const password = form.get("password");
+  const redirectTo = form.get("redirectTo") || "/jokes";
+  if (
+    typeof loginType !== "string" ||
+    typeof username !== "string" ||
+    typeof password !== "string" ||
+    typeof redirectTo !== "string"
+  ) {
+    return badRequest({
+      formError: `Form not submitted correctly.`,
+    });
+  }
+
+  const fields = { loginType, username, password };
+  const fieldErrors = {
+    username: validateUsername(username),
+    password: validatePassword(password),
+  };
+  if (Object.values(fieldErrors).some(Boolean))
+    return badRequest({ fieldErrors, fields });
+
+  switch (loginType) {
+    case "login": {
+      // login to get the user
+      const user = await login({ username, password });
+      console.log({ user });
+      if (!user) {
+        return badRequest({
+          fields,
+          formError: `Invalid username or password`,
+        });
+      }
+      // if there is a user, create their session and redirect to /jokes
+      return createUserSession(user.id, redirectTo);
+      // return badRequest({
+      //   fields,
+      //   formError: "Not implemented",
+      // });
+    }
+    case "register": {
+      const userExists = await db.user.findFirst({
+        where: { username },
+      });
+      if (userExists) {
+        return badRequest({
+          fields,
+          formError: `User with username ${username} already exists`,
+        });
+      }
+      // create the user
+      // create their session and redirect to /jokes
+      return badRequest({
+        fields,
+        formError: "Not implemented",
+      });
+    }
+    default: {
+      return badRequest({
+        fields,
+        formError: `Login type invalid`,
+      });
+    }
+  }
+};
+
+export default function Login() {
+  const actionData = useActionData<ActionData>();
+  const [searchParams] = useSearchParams();
+  return (
+    <div className="container">
+      <div className="content" data-light="">
+        <h1>Login</h1>
+        <form
+          method="post"
+          aria-describedby={
+            actionData?.formError ? "form-error-message" : undefined
+          }
+        >
+          <input
+            type="hidden"
+            name="redirectTo"
+            value={searchParams.get("redirectTo") ?? undefined}
+          />
+          <fieldset>
+            <legend className="sr-only">Login or Register?</legend>
+            <label>
+              <input
+                type="radio"
+                name="loginType"
+                value="login"
+                defaultChecked
+              />{" "}
+              Login
+            </label>
+            <label>
+              <input type="radio" name="loginType" value="register" /> Register
+            </label>
+          </fieldset>
+          <div>
+            <label htmlFor="username-input">Username</label>
+            <input type="text" id="username-input" name="username" />
+          </div>
+          <div>
+            <label htmlFor="password-input">Password</label>
+            <input id="password-input" name="password" type="password" />
+          </div>
+          <button type="submit" className="button">
+            Submit
+          </button>
+        </form>
+      </div>
+      <div className="links">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/jokes">Jokes</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
